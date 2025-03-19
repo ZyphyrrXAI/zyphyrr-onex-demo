36612fbac1e7d9f6125513f3ee5e5bfdd48b94db
Louie A. Tabaranza <supermanx2025ai@gmail.com>
2025-03-19 23:08:46 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("github-link").addEventListener("click", function() {
+    window.open("https://github.com/YourUsername/YourRepository", "_blank");
+});
+
+document.getElementById("live-demo-link").addEventListener("click", function() {
+    window.open("https://YourUsername.github.io/YourRepository", "_blank");
+});

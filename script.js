04e8cba415245c0e7d0712c001702d17486955ee
Louie A. Tabaranza <supermanx2025ai@gmail.com>
2025-03-19 23:15:38 +0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("github-link").addEventListener("click", function() {
-    window.open("https://github.com/YourUsername/YourRepository", "_blank");
-});
-
-document.getElementById("live-demo-link").addEventListener("click", function() {
-    window.open("https://YourUsername.github.io/YourRepository", "_blank");
-});
